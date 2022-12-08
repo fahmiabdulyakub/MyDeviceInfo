e89793dfe67574409b48110aa8ea1ae1ed03e153
fahmiabdulyakub <fahmiabdulyakub28@gmail.com>
2022-12-08 22:56:43 +0700

    create themes

---
+++ src/themes/Colors.ts
@@ -0,0 +1,14 @@
+const Colors = {
+  light: '#f8f9fa',
+  blue: '#1BA0E2',
+  black: '#020203',
+  white: '#FFFFFF',
+  light_grey: '#eaeaea',
+  background: '#F9F9F9',
+  green: '#479647',
+  gray: '#C2C2C8',
+  dark_gray: '#6c757d',
+  red: '#D7463C',
+};
+
+export default Colors;
