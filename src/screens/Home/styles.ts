87d614fc457b8bc5cf7f3cc5e237895df2bac6f8
fahmiabdulyakub <fahmiabdulyakub28@gmail.com>
2022-12-08 22:57:59 +0700

    create navigation

---
+++ src/screens/Home/styles.ts
@@ -0,0 +1,14 @@
+import {StyleSheet} from 'react-native';
+import {Colors} from 'themes';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: Colors.white,
+  },
+  title: {
+    color: Colors.black,
+  },
+});
+
+export default styles;
